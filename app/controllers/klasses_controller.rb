91524e5bb3674a382fe1efc2db5a67029cfd151e
Aaqib Parvez <aaqib29292@gmail.com>
2016-03-16 18:14:39 +0530

    Create, add and list Klasses

---
+++ app/controllers/klasses_controller.rb
@@ -0,0 +1,27 @@
+class KlassesController < ApplicationController
+
+  def index
+    @klasses = Klass.all
+    #instantiate new object
+    @new_klass = Klass.new
+  end
+
+  def create
+    #instantiate new object
+    @klass = Klass.new
+    @klass.name = params[:klass][:name]
+    #save the new object
+    @save_success = @klass.save # .save returns true or false
+    if @klass.save
+    else
+    end
+  end
+end
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # The priority is based upon order of creKlassesation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
---
+++ app/models/klass.rb
@@ -0,0 +1,4 @@
+class Klass < ActiveRecord::Base
+  validates :name, presence: true
+
+end
