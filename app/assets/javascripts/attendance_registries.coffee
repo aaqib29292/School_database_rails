96e4538c2e6b13ebf18c3dc1d463f1850829bb0e
Aaqib Parvez <aaqib29292@gmail.com>
2016-03-19 01:48:33 +0530

    Absentees saved to database when attendance is taken

--- app/assets/javascripts/attendance_registries.coffee
+++ app/assets/javascripts/attendance_registries.coffee
@@ -11,7 +11,7 @@
       absentees_ids.push(ele.dataset.studentId))
     console.log absentees_ids
     $("#attendance_registry_absentee_ids").val(absentees_ids.join(','))
-    $("#attendance_registry_form")[0].submit()
+    $("#attendance_registry_form input.submit").click()
 
 $ ->
   $('a#submit-attendance').click (ev) ->
--- app/controllers/attendance_registries_controller.rb
+++ app/controllers/attendance_registries_controller.rb
@@ -3,13 +3,13 @@ class AttendanceRegistriesController < ApplicationController
   before_action :fetch_klass_section
 
   skip_before_filter :verify_authenticity_token
+
   def new
     @students_attendance = @section.students
     @attendance_registry = @section.attendance_registries.new
   end
 
   def create
-    debugger
     @attendance_registry = @section.attendance_registries.new
     @attendance_registry.date = Date.today
     @attendance_registry.absentee_ids = params[:attendance_registry][:absentee_ids].split(",").collect(&:to_i)
