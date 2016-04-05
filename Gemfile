f67ebb4f27f908eb791e6d40fc8f979c4d5bf078
Aaqib Parvez <aaqib29292@gmail.com>
2016-04-05 11:18:15 +0530

    securing API using token at env stage

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
   gem 'byebug'
+  gem 'dotenv-rails
 end
 
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,20 @@
-class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-    skip_before_action :verify_authenticity_token, if: :api_request?, only: [:index]
 
-protected
-  def api_request?
-    request.format.json? || request.format.xml?
+  class ApplicationController < ActionController::Base
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+    skip_before_action :verify_authenticity_token, if: :api_request?
+
+    before_action :authorize_access!
+
+  protected
+    def api_request?
+      params[:controller].split("/").first == "api"
+    end
+
+    def authorize_access!
+      return unless api_request?
+      head :unauthorized unless params[:access_token] == ENV["API_ACCESS_TOKEN"]
+      return false
+    end
   end
-end
